fa5c9e3f8b7d32e27505d14d633b8952f7716571
Ayshazaha <ayshazaha05@gmail.com>
2026-06-08 14:21:41 +0530

    feat: register payment routes

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const orderRoutes = require("./routes/orderRoutes");
-
+const paymentRoutes = require("./routes/paymentRoutes");
 const app = express();
 
 app.use(cors());
@@ -16,7 +16,7 @@ app.get("/", (req, res) => {
 
 // Orders API
 app.use("/api/orders", orderRoutes);
-
+app.use("/api/payment", paymentRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
